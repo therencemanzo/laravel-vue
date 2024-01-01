134fdcb590e31fcbb7cb8137f5b368d285ef4d48
Therencertm <129299007+Therencertm@users.noreply.github.com>
2024-01-01 21:21:20 +0000

    update set up

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,7 +12,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        //\App\Models\User::factory(1)->admin()->create();
+        \App\Models\User::factory(1)->admin()->create();
         \App\Models\Customer::factory(20)->create();
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
